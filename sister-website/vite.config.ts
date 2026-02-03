b63e5646e8610a6827b10cd3a697600f0711be74
yo-yo0613 <chengyouli37@gmail.com>
2026-02-03 14:10:54 +0800

    add sitemap.xml

--- sister-website/vite.config.ts
+++ sister-website/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://xungfoodie.web.app', // 你的網站網址
+      dynamicRoutes: [
+        '/', 
+        '/travel', 
+        '/post', // 如果你有固定路由，可以手動列出
+        'taipei',
+        'newTaipei',
+        'taichung',
+        'contact',
+      ],
+      // 如果你的食記是從 API 抓的，稍後我們可以再討論如何抓取動態 ID
+    }),
+  ],
+});
